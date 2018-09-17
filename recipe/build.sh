17198bdc8818678ed4fb14bfd593fec5793db9d0
Duncan Macleod <duncan.macleod@ligo.org>
2018-09-17 19:24:15 +0000

    recipe: hard-code lib-dir to lib (not lib64)

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,9 @@ find . -name CMakeLists.txt
 mkdir -p build
 pushd build
 
-cmake .. -DCMAKE_INSTALL_PREFIX=${PREFIX}
+cmake .. \
+	-DCMAKE_INSTALL_PREFIX=${PREFIX} \
+	-DCMAKE_INSTALL_LIBDIR="lib"
 cmake --build . --config Release -- -j${CPU_COUNT}
 ctest -V
 cmake --build . --config Release --target install
