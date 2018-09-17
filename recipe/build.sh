00c09593e4bc0af0cc2294f8d57b5ab613aed1b1
Duncan Macleod <duncan.macleod@ligo.org>
2018-09-17 17:01:30 +0100

    recipe: build using cmake patch
    - should enable windows build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,14 @@
-./configure --prefix=${PREFIX}
-make -j ${CPU_COUNT}
-make install
+#!/bin/bash
+
+# copy CMakeLists.txt from the recipe into the src
+cp ${RECIPE_DIR}/CMakeLists.txt .
+
+mkdir -p build
+pushd build
+
+cmake .. \
+	-DCMAKE_INSTALL_PREFIX=${PREFIX} \
+	-DCMAKE_INSTALL_LIBDIR="lib"
+cmake --build . --config Release -- -j${CPU_COUNT}
+ctest -V
+cmake --build . --config Release --target install
